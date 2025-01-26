ca7543f1a91665617abe6a51ee6c59293b06c799
Kwesijay1 <adjeigideon911@gmail.com>
2025-01-26 00:33:04 +0000

    initial

--- src/main/java/com/springboot/citestingapp/repository/UserRepository.java
+++ src/main/java/com/springboot/citestingapp/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.springboot.citestingapp.repository;
 
-import springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.springboot.citestingapp.model.User;
 
-public interface UserRepository extends com.springboot.citestingapp.repository.JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
